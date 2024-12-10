b7c61b9151d6765aa2adb9d10323ec8483564537
Liam Davis <wjdavisiii@pm.me>
2024-12-10 12:29:19 -0500

    Enhance Astro configuration with Vercel adapter and update dependencies
    - Added Vercel adapter to the Astro configuration for deployment.
    - Updated package.json to include the new Vercel dependency.
    - Cleaned up the Astro configuration file for better readability.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
-import tailwind from '@astrojs/tailwind';
-
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
+import sitemap from '@astrojs/sitemap'
+import vercel from '@astrojs/vercel'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap()]
-});
+	integrations: [tailwind(), sitemap()],
+	adapter: vercel()
+})
